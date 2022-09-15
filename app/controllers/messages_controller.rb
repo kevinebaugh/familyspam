791abb5760790f2c0ea9e536010bb05b803cc0c9
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-09-15 12:46:26 -0400

    Find relevant groups from email aliases

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,8 +15,10 @@ class MessagesController < ApplicationController
 
   # POST /messages
   def create
+    render json: "Nobody found", status: :not_found and return unless group_ids_from_aliases&.any?
+
     @message = Message.new(
-      group_id: group_id_from_alias,
+      group_id: group_ids_from_aliases,
       raw_content: nil
     )
 
@@ -52,8 +54,19 @@ class MessagesController < ApplicationController
       params.fetch(:message, {})
     end
 
-    def group_id_from_alias
-      incoming_alias = params["To"].split(" ")[1].split("@").first[1..]
-      Group.find_by(email_alias: incoming_alias).id
+    def group_ids_from_aliases
+      # params["recipient"] sometimes has multiple email addresses, so split it just in case.
+      # So far, haven't seen a name attached to params["recipient"] so not worrying about that for now.
+      # Examples:
+      # • anything@sandboxc07be1039bd545938ea0cc91b46bfe65.mailgun.org
+      # • anything@sandboxc07be1039bd545938ea0cc91b46bfe65.mailgun.org,anything-bcc@sandboxc07be1039bd545938ea0cc91b46bfe65.mailgun.org,anything-to@sandboxc07be1039bd545938ea0cc91b46bfe65.mailgun.org
+
+      incoming_aliases = params["recipient"].split(",").map{ |address| address.split("@")[0] }
+
+      group_ids_to_message = incoming_aliases.map do |incoming_alias|
+        Group.find_by(email_alias: incoming_alias)&.id
+      end&.compact&.uniq
+
+      group_ids_to_message
     end
 end
