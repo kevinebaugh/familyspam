0d1a215e049e8043a89bd4c3a2752028be7bd94b
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-09-14 20:46:13 -0400

    Start to build message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,10 @@ class MessagesController < ApplicationController
 
   # POST /messages
   def create
-    @message = Message.new(message_params)
+    @message = Message.new(
+      group_id: group_id_from_alias,
+      raw_content: nil
+    )
 
     if @message.save
       render json: @message, status: :created, location: @message
@@ -47,5 +50,10 @@ class MessagesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def message_params
       params.fetch(:message, {})
+    end
+
+    def group_id_from_alias
+      incoming_alias = params["To"].split(" ")[1].split("@").first[1..]
+      Group.find_by(email_alias: incoming_alias).id
     end
 end
