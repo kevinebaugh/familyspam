f7f4fdd1dae0f0bb434e04b8cd15e1ba5d01ebf5
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-10-20 23:48:03 -0400

    Features

--- client/src/SignInUp.js
+++ client/src/SignInUp.js
@@ -12,14 +12,10 @@ function SignInUp( {user, setUser} ) {
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
-  const [signingUp, setSigningUp] = useState(true)
+  const [status, setStatus] = useState("learning")
 
   const [showToast, setShowToast] = useState(false)
   const [toastBody, setToastBody] = useState("")
-
-  function toggleSignUpIn() {
-    setSigningUp(!signingUp)
-  }
 
   function handleSignup(e) {
     e.preventDefault()
@@ -74,14 +70,20 @@ function SignInUp( {user, setUser} ) {
       })
   }
 
+  function changeStatusTo(e) {
+    e.preventDefault()
+
+    setStatus(e.target.id)
+  }
+
   if (user) {
     console.log("Signed in, redirecting")
     return <Redirect to='/group-management'  />
   }
 
-  if (signingUp === true) {
-    return (
-      <>
+  return (
+    <>
+      <Stack gap={2} className="col-md-5 mx-auto">
         <ToastContainer position="top-end" className="p-3">
           <Toast
             onClose={() => setShowToast(false)}
@@ -98,118 +100,127 @@ function SignInUp( {user, setUser} ) {
             </Toast.Body>
           </Toast>
         </ToastContainer>
-        <Stack gap={2} className="col-md-5 mx-auto">
-          <img style={{width: "20%"}} src={letter} />
-          <h1>Family Spam</h1>
-          <h3>Features:</h3>
-          <ul>
-            <li>One email address that forwards to your whole family.</li>
-            <li>Simple group management.</li>
-            <li>Straightforward pricing: <s>$1 per 100 emails</s> free for now </li>
-          </ul>
 
-          <hr/>
-          <h3>Sign up for a new account:</h3>
-          <Form onSubmit={handleSignup}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                onChange={(e) => setEmailAddress(e.target.value)}
-                autofocus
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+        {status === "learning" ? (
+          <>
+            <img style={{width: "20%"}} src={letter} />
+            <h1>Family Spam</h1>
+            <h3>Features:</h3>
+            <ul>
+              <li>One email address that forwards to your whole family.</li>
+              <li>Simple group management.</li>
+              <li>Straightforward pricing: <s>$1 per 100 emails</s> free for now </li>
+            </ul>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPasswordConfirmation">
-              <Form.Label>Password confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password confirmation"
-                onChange={(e) => setPasswordConfirmation(e.target.value)}
-              />
-            </Form.Group>
             <Button
+              size="lg"
               variant="outline-primary"
               type="submit"
-              disabled={
-                emailAddress === "" || password === "" || passwordConfirmation === ""
-              }
+              onClick={changeStatusTo}
+              id="signingUp"
             >
               Sign up
             </Button>
-          </Form>
-          <hr/>
-          <h4>Already have an account? <Button variant="outline-secondary" onClick={toggleSignUpIn}>Sign in</Button></h4>
-        </Stack>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <ToastContainer position="top-end" className="p-3">
-            <Toast
-              onClose={() => setShowToast(false)}
-              show={showToast}
-              delay={5000}
-              autohide
-              bg={"danger"}
-            >
-              <Toast.Header>
-                <strong className="me-auto">FamilySpam 🤖</strong>
-              </Toast.Header>
-              <Toast.Body className={"text-white"}>
-                {toastBody}
-              </Toast.Body>
-            </Toast>
-          </ToastContainer>
-        <Stack gap={2} className="col-md-5 mx-auto">
-          <h3>Sign in to your account:</h3>
-          <Form onSubmit={handleSignin}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                onChange={(e) => setEmailAddress(e.target.value)}
-              />
-            </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
             <Button
-              variant="outline-primary"
+              size="lg"
+              variant="outline-secondary"
               type="submit"
-              disabled={
-                emailAddress === "" || password === ""
-              }
+              onClick={changeStatusTo}
+              id="signingIn"
             >
               Sign in
             </Button>
-          </Form>
-          <hr/>
-          <h4>Haven't signed up yet? <Button variant="outline-secondary" onClick={toggleSignUpIn}>Sign up</Button></h4>
-        </Stack>
-      </>
-    )
-  }
+          </>
+        ) : null }
+
+        {status === "signingUp" ? (
+          <>
+            <h3>Sign up for a new account:</h3>
+            <Form onSubmit={handleSignup}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  onChange={(e) => setEmailAddress(e.target.value)}
+                  autofocus
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPasswordConfirmation">
+                <Form.Label>Password confirmation</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password confirmation"
+                  onChange={(e) => setPasswordConfirmation(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                size="lg"
+                variant="outline-primary"
+                type="submit"
+                disabled={
+                  emailAddress === "" || password === "" || passwordConfirmation === ""
+                }
+              >
+                Sign up
+              </Button>{" "}
+              <span> or <a onClick={changeStatusTo} id="signingIn" href="#">sign in</a></span>
+            </Form>
+          </>
+        ) : null }
+
+        {status === "signingIn" ? (
+          <>
+            <h3>Sign in to your account:</h3>
+            <Form onSubmit={handleSignin}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  onChange={(e) => setEmailAddress(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                size="lg"
+                variant="outline-primary"
+                type="submit"
+                disabled={
+                  emailAddress === "" || password === ""
+                }
+              >
+                Sign in
+              </Button>
+
+              <span> or <a onClick={changeStatusTo} id="learning" href="#">learn more</a> before <a onClick={changeStatusTo} id="signingUp" href="#">signing up</a></span>
+            </Form>
+          </>
+        ) : null }
+      </Stack>
+    </>
+  )
 }
 
 export default SignInUp;
